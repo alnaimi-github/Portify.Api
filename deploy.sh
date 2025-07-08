77c259b227cdaed10c9b24d15c104abd5639e2ce
Abdulwaisa Al Nuaimi <alnymybdalwas1@gmail.com>
2025-07-08 23:14:39 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 set -ex
 
-# Install Docker if not installed
+# Install Docker the Amazon Linux way if not present
 if ! command -v docker &> /dev/null; then
   echo "Docker not found. Installing Docker..."
   sudo yum update -y
-  sudo yum install -y yum-utils
-  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-  sudo yum install -y docker-ce docker-ce-cli containerd.io
+  sudo yum install -y docker
   sudo systemctl enable docker
   sudo systemctl start docker
   sudo usermod -aG docker ec2-user
-  # You may need to reboot or log out/in for group changes to take effect
 fi
 
 cd /home/ec2-user/project
